393722b47d260f0041a398df641494417a0c354c
billy3b <avinashroy45678@gmail.com>
2023-05-05 00:13:29 +0530

    fixed user profile icon

--- components/Header.js
+++ components/Header.js
@@ -2,8 +2,8 @@
 
 import React from "react";
 import Image from "next/image";
-import HeaderIcon from "./HeaderIcon";
 import { useSession,} from "next-auth/react"
+import HeaderIcon from "./HeaderIcon";
 
 import {
   BellIcon,
@@ -21,12 +21,12 @@ import {
   ShoppingCartIcon,
   FireIcon
 } from "@heroicons/react/24/outline";
-import { signOut } from "next-auth/react";
+
 
 const Header = () => {
 
   const session = useSession();
-  const source = session?.data?.user?.image;
+
 
   return (
     <div className="flex sticky top-0 z-50 bg-white lg:px-5 shadow-md items-center justify-start">
@@ -41,23 +41,21 @@ const Header = () => {
       {/* center */}
       <div className="flex justify-center flex-grow">
         <div className="flex space-x-6 md: space-x-2">
-          <HeaderIcon active Icon={HomeIcon}/>
-          <HeaderIcon Icon={FlagIcon}/>
-          <HeaderIcon Icon={PlayIcon}/>
-          <HeaderIcon Icon={ShoppingCartIcon}/>
-          <HeaderIcon Icon={UserGroupIcon}/>
+          <HeaderIcon active icons={HomeIcon}/>
+          <HeaderIcon icons={FlagIcon}/>
+          <HeaderIcon icons={PlayIcon}/>
+          <HeaderIcon icons={ShoppingCartIcon}/>
+          <HeaderIcon icons={UserGroupIcon}/>
         </div>
       </div>
       {/* right */}
       <div className="flex items-center justify-end sm: space-x-2">  
       {/* profilepic */}
       
-      <Image src={source}
-      width={40}
-      height={40}
+      <HeaderIcon src={session?.data?.user?.image}
       alt="profilepic" 
       className="rounded-full"
-      onClick={signOut} /> 
+       /> 
 
         <p className="whitespace-nowrap font-semibold pr-3 cursor-pointer"
           
