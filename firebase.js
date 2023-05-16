6f15511378ff03f055a5243f64e531453ae82adc
billy3b <avinashroy45678@gmail.com>
2023-05-16 17:50:09 +0530

    debugging 2

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import { initializeApp } from 'firebase/app';
 import { getFirestore, } from "firebase/firestore";
@@ -27,33 +26,3 @@ const storage = getStorage();
 export {db,storage};
 
   
-=======
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-import { initializeApp } from 'firebase/app';
-import { getFirestore, } from "firebase/firestore";
-import { getStorage,} from "firebase/storage";
-
-
-const firebaseConfig = {
-    apiKey: "*****************************",
-    authDomain: "*****************************",
-    projectId: "*****************************",
-    storageBucket: "*****************************",
-    messagingSenderId: "*****************************",
-    appId: "*****************************",
-    measurementId: "*****************************"
-  };
-
-  //init firebase
-initializeApp(firebaseConfig);
-
-//init services
-const db = getFirestore();
-
-const storage = getStorage();
-
-
-export {db,storage};
-
-  
->>>>>>> 26e1927e58f72519fafad6944add31e76263042e
--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -12,18 +11,3 @@ const nextConfig = {
   },
 }
 module.exports = nextConfig;
-=======
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-      },
-    ],   
-  },
-}
-module.exports = nextConfig;
->>>>>>> 26e1927e58f72519fafad6944add31e76263042e
