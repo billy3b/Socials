1dbcc0f754fc92ea970f89ecb447ace113d2aa1a
Avinash Roy <108816279+billy3b@users.noreply.github.com>
2023-06-03 11:03:04 +0530

    Update HeaderIcon.js

--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { signOut } from "next-auth/react";
 
-const HeaderIcon = ({Icon, active}) => {
+
+const HeaderIcon = ({ icons, src }) => {
   return (
-    <div className=' cursor-pointer md:hover:bg-gray-100 px-10 sm:h-14 rounded-xl active:border-b-2 active:border-blue-500'>
-      <Icon className={`h-5 text-gray-500 group-hover:text-blue-500 sm:h-7 mx-auto ${active &&'text-blue-500'}`}/>
+    <div> 
+      {src &&  
+        <img src={src} alt='profilepic' className='mt-9 flex justify-end justify-center w-10 h-10 rounded-xl'  onClick={signOut}/>}
+        <div className=' cursor-pointer md:hover:bg-gray-100 px-10 sm:h-14 rounded-xl active:border-b-2 active:border-blue-500'>
+          <icons className='h-5 text-gray-500 group-hover:text-blue-500 sm:h-7 mx-auto '/>
+      </div>
     </div>
+  
   )
 }
 
-export default HeaderIcon
+export default HeaderIcon;
